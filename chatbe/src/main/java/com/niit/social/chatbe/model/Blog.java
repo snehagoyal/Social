65d371816ccbc6395ea67e388e1494f9edefa9ea
snehagoyal <snehagoyal2016@gmail.com>
2017-06-02 18:33:23 +0530

    user

--- chatbe/src/main/java/com/niit/social/chatbe/model/Blog.java
+++ chatbe/src/main/java/com/niit/social/chatbe/model/Blog.java
@@ -3,60 +3,23 @@ package com.niit.social.chatbe.model;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
 import org.hibernate.validator.constraints.br.CNPJ;
 
+@Table(name="c_blog")
+@Entity
 public class Blog extends basedomain {
-@javax.persistence.Id
-	private Integer Id;
-	private String title;
-	public Integer getId() {
-		return Id;
-	}
-	public void setId(Integer id) {
-		Id = id;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getDesc() {
-		return desc;
-	}
-	public void setDesc(String desc) {
-		this.desc = desc;
-	}
-	public String getUserID() {
-		return userID;
-	}
-	public void setUserID(String userID) {
-		this.userID = userID;
-	}
-	public Date getDateTime() {
-		return dateTime;
-	}
-	public void setDateTime(Date dateTime) {
-		this.dateTime = dateTime;
-	}
-	public String getReason() {
-		return reason;
-	}
-	public void setReason(String reason) {
-		this.reason = reason;
-	}
-	public char getStatus() {
-		return status;
-	}
-	public void setStatus(char status) {
-		this.status = status;
-	}
-	private String desc;
-	@Column( name="User_Id")
-	private String userID;
+
+@Id
+	private String b_id;
+	private String b_title;
+	private String b_desc;
+
+
 	
 	@Transient
 	@Column(name=" Date_time")
